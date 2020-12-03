262c3e2b14f65df1a661acdb2b0b7662f1cb70e4
Alin Pisica <Alin.Pisica@exghts.com>
2020-12-03 14:39:36 +0200

    Days 2 & 3

---
+++ Day02/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Day02
+{
+    class Program
+    {
+        static bool CheckPasswordValidationCounter(string password, char charToInclude, int minOccurences, int maxOccurences)
+        {
+            int countOfOccurences = Regex.Matches(password, charToInclude.ToString()).Count;
+
+            return countOfOccurences >= minOccurences && countOfOccurences <= maxOccurences;
+        }
+
+        static bool CheckPasswordValidationOnPositions(string password, char charToInclude, int firstIndex, int secondIndex)
+        {
+            if (secondIndex >= password.Length)
+            {
+                return password[firstIndex] == charToInclude;
+            }
+
+            if (firstIndex < 0)
+            {
+                return password[secondIndex] == charToInclude;
+            }
+
+            return (password[firstIndex] == charToInclude) ^ (password[secondIndex] == charToInclude);
+        }
+
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"input.txt");
+
+            int numberOfValidPasswordsPartOne = 0;
+            int numberOfValidPasswordsPartTwo = 0;
+
+            foreach (string line in lines)
+            {
+                string[] splittedLine = line.Split(":");
+                string[] conditions = splittedLine[0].Split(" ");
+                string password = splittedLine[1].Trim();
+
+                char substringToInclude = conditions[1][0];
+                string[] range = conditions[0].Split("-");
+
+                int minNumber = Int32.Parse(range[0]);
+                int maxNumber = Int32.Parse(range[1]);
+
+                numberOfValidPasswordsPartOne += CheckPasswordValidationCounter(password, substringToInclude, minNumber, maxNumber) 
+                                                                ? 1 
+                                                                : 0;
+                numberOfValidPasswordsPartTwo += CheckPasswordValidationOnPositions(password, substringToInclude, minNumber - 1, maxNumber - 1) 
+                                                                ? 1 
+                                                                : 0;
+            }
+
+            Console.WriteLine("Part One: " + numberOfValidPasswordsPartOne);
+            Console.WriteLine("Part Two: " + numberOfValidPasswordsPartTwo);
+
+            return;
+        }
+    }
+}
---
+++ Day03/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Day03
+{
+    class Program
+    {
+        static int treesOnSlope(string[] lines, int slopeX, int slopeY)
+        {
+            int lineLength = lines[0].Length;
+
+            int x = 0;
+            int y = 0;
+            int trees = 0;
+
+            while (y < lines.Length)
+            {
+                trees += lines[y][x] == '#' ? 1 : 0;
+
+                x = (x + slopeX) % lineLength;
+                y += slopeY;
+            }
+
+            return trees;
+        }
+
+        static int solvePartOne(string[] lines)
+        {
+            return treesOnSlope(lines, 3, 1);
+        }
+
+        static long solvePartTwo(string[] lines)
+        {
+            int totalTrees = 1;
+
+            totalTrees *= treesOnSlope(lines, 1, 1);
+            totalTrees *= treesOnSlope(lines, 3, 1);
+            totalTrees *= treesOnSlope(lines, 5, 1);
+            totalTrees *= treesOnSlope(lines, 7, 1);
+            totalTrees *= treesOnSlope(lines, 1, 2);
+
+            return totalTrees;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"input.txt");
+
+            Console.WriteLine("Part One: " + solvePartOne(lines));
+            Console.WriteLine("Part Two: " + solvePartTwo(lines));
+        }
+    }
+}
